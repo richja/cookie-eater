cc85726798cb46381b03b7386377bd1e6ca02f43
Jan Richter <rich2@seznam.cz>
2020-01-26 15:02:00 +0000

    add cookie counter using local storage

--- dough.js
+++ dough.js
@@ -1,13 +1,16 @@
 document.addEventListener("DOMContentLoaded", (event) => { 
+    setCounter(getCounter())
+    
     document.getElementById("tray").addEventListener( "click", () => {
         toggleCookieEater()
         changeFavicon("favicon-yummy.ico")
+
         setTimeout(() => {
             toggleCookieEater()
-        changeFavicon("favicon-yummy.ico")
             changeFavicon("favicon.ico")
+            updateCounter()
         }, 1000)
-    }, false );
+    }, false )
 })
 
 function toggleCookieEater() {
@@ -17,4 +20,25 @@ function toggleCookieEater() {
 
 function changeFavicon(name) {
     document.getElementById("favicon").setAttribute("href", name)
+}
+
+function setCounter(counter) {
+    document.getElementById("cookie-counter").innerText = buildContent(counter)
+}
+
+function buildContent(counter) {
+    return (counter === 1) ? "1 cookie" : `${counter} cookies` 
+}
+
+function updateCounter() {
+    let counter = getCounter()
+    localStorage.setItem("counter", ++counter)
+    setCounter(counter);
+}
+
+function getCounter() {
+    if (typeof(Storage) !== "undefined") {
+        return localStorage.counter ? localStorage.counter : 0
+    }
+    return 0
 }
