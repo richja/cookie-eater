b001711b0812046a70dcfa0eff719105e5d9d497
Jan Richter <rich2@seznam.cz>
2020-01-31 20:17:33 +0000

    fetch global counter on page load

--- dough.js
+++ dough.js
@@ -1,18 +1,13 @@
-
 document.addEventListener("DOMContentLoaded", (event) => { 
     let db = initFireFirebase()
     let counterRef = db.collection("hits").doc("counter")
 
     setCounter(getCounter())
-    getGlobalCounter(counterRef).then((result) => {
-        // use the result here
-        setCounter(result)
-        console.log(result)
-    })
 
     counterRef.onSnapshot((doc) => {
-        const source = doc.metadata.hasPendingWrites ? "Local" : "Server"
-        console.log(source, " data: ", doc.data())
+        getGlobalCounter(counterRef).then((result) => {
+            setGlobalCounter(result)
+        })
     })
     
     document.getElementById("tray").addEventListener("click", () => {
@@ -22,7 +17,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
         setTimeout(() => {
             toggleCookieEater()
             changeFavicon("favicon.ico")
-            updateCounter()
+            incrementCounter()
             incrementGlobalCounter(counterRef)
         }, 1000)
     }, false)
@@ -41,55 +36,22 @@ function setCounter(counter) {
     document.getElementById("cookie-counter").innerText = buildContent(counter)
 }
 
-function buildContent(counter) {
-    return (counter === 1) ? "1 cookie" : `${counter} cookies` 
+function setGlobalCounter(counter) {
+    document.getElementById("cookie-counter-global").innerText = buildContent(counter)
 }
 
-function updateCounter() {
+
+
+function incrementCounter() {
     let counter = getCounter()
     localStorage.setItem("counter", ++counter)
     setCounter(counter) 
-}
-
-function getCounter() {
-    if (typeof(Storage) !== "undefined") {
-        return localStorage.counter ? localStorage.counter : 0
-    }
-    return 0
-}
-
-function initFireFirebase() {
-    firebase.initializeApp({
-        apiKey: "AIzaSyBuJUN6DocXhvzE4bVj327a3R80fh1qei8",
-        authDomain: "cookie-eater.firebaseapp.com",
-        databaseURL: "https://cookie-eater.firebaseio.com",
-        projectId: "cookie-eater",
-    })
-
-    // Initialize Firebase
-    return firebase.firestore()
-}
-
-function getGlobalCounter(ref) {
-    return ref.get().then((doc) => {
-        if (doc.exists) {
-            console.log(doc.data().total)
-            return doc.data().total
-        } else {
-            console.log("No such document!")
-            return 0
-        }
-    }).catch((error) => {
-        console.log("Error getting document:", error)
-    })
 }
 
 function incrementGlobalCounter(ref) {
     ref.get().then((doc) => {
         if (doc.exists) {
             let counterValue = doc.data().total
-            console.log("Document data:", doc.data().total)
-
             ref.set({
                 total: ++counterValue
             }).then(() => {
@@ -101,4 +63,39 @@ function incrementGlobalCounter(ref) {
     }).catch((error) => {
         console.log("Error getting document:", error)
     })
+}
+
+function getCounter() {
+    if (typeof(Storage) !== "undefined") {
+        return localStorage.counter ? localStorage.counter : 0
+    }
+    return 0
+}
+
+function getGlobalCounter(ref) {
+    return ref.get().then((doc) => {
+        if (doc.exists) {
+            return doc.data().total
+        } else {
+            console.log("No such document!")
+            return 0
+        }
+    }).catch((error) => {
+        console.log("Error getting document:", error)
+    })
+}
+
+function buildContent(counter) {
+    return (counter === 1) ? "1 cookie" : `${counter} cookies` 
+}
+
+function initFireFirebase() {
+    firebase.initializeApp({
+        apiKey: "AIzaSyBuJUN6DocXhvzE4bVj327a3R80fh1qei8",
+        authDomain: "cookie-eater.firebaseapp.com",
+        databaseURL: "https://cookie-eater.firebaseio.com",
+        projectId: "cookie-eater",
+    })
+
+    return firebase.firestore()
 }
